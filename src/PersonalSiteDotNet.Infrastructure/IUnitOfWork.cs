f79967adca584543f3b94bf009d8fefa1a332db6
Cherry Goh <cherrycecegoh@gmail.com>
2020-05-31 03:50:40 +0800

    feat: add Unit of Work interface and class

---
+++ src/PersonalSiteDotNet.Infrastructure/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using PersonalSiteDotNet.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonalSiteDotNet.Infrastructure
+{
+    public interface IUnitOfWork
+    {
+        IRepository<Blog> BlogRepository { get; }
+        IRepository<Entry> EntryRepository { get; }
+        IRepository<Tag> TagRepository { get; }
+        IRepository<EntryTag> EntryTagRepository { get; }
+        void Save();
+    }
+}
---
+++ src/PersonalSiteDotNet.Infrastructure/UnitOfWork.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PersonalSiteDotNet.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonalSiteDotNet.Infrastructure
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        DbContext _dbContext;
+        IRepository<Blog> _blogRepository;
+        IRepository<Entry> _entryRepository;
+        IRepository<Tag> _tagRepository;
+        IRepository<EntryTag> _entryTagRepository;
+
+        public UnitOfWork(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IRepository<Blog> BlogRepository
+        {
+            get
+            {
+                return _blogRepository ?? (new Repository<Blog>(_dbContext));
+            }
+            
+        }
+
+        public IRepository<Entry> EntryRepository
+        {
+            get
+            {
+                return _entryRepository ?? (new Repository<Entry>(_dbContext));
+            }
+        }
+
+        public IRepository<Tag> TagRepository
+        {
+            get
+            {
+                return _tagRepository ?? (new Repository<Tag>(_dbContext));
+            }
+        }
+
+        public IRepository<EntryTag> EntryTagRepository
+        {
+            get
+            {
+                return _entryTagRepository ?? (new Repository<EntryTag>(_dbContext));
+            }
+        }
+
+        public void Save()
+        {
+            _dbContext.SaveChanges();
+        }
+    }
+}
