698c7b254d82d1a0a27b26b5ce70c462a1aaec7e
Cherry Goh <cherrycecegoh@gmail.com>
2020-06-01 00:26:20 +0800

    test: re-setup Infrastructure test project

---
+++ test/PersonalSiteDotNet.Infrastructure.Tests/MockEntity.cs
@@ -0,0 +1,13 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonalSiteDotNet.Infrastructure.Tests
+{
+    public class MockEntity
+    {
+        public int Id;
+        public string Name;
+    }
+}
