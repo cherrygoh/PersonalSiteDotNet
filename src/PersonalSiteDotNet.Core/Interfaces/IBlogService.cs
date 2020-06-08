2c947ce5c9c93d5f2c0204d130009c73c6b73cc1
Cherry Goh <cherrycecegoh@gmail.com>
2020-06-09 02:46:33 +0800

    refactor: move IBlogService to Interfaces folder

--- src/PersonalSiteDotNet.Core/Interfaces/IBlogService.cs
+++ src/PersonalSiteDotNet.Core/Interfaces/IBlogService.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PersonalSiteDotNet.Core.Services
+namespace PersonalSiteDotNet.Core.Interfaces
 {
     public interface IBlogService
     {
