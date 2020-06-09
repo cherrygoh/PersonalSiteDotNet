6d4f57afedcd93aa16d8010ebe3d3059b29f35f9
Cherry Goh <cherrycecegoh@gmail.com>
2020-06-09 14:44:08 +0800

    feat: configure DI for infrastructure layer and blog service

--- src/PersonalSiteDotNet.Web/Startup.cs
+++ src/PersonalSiteDotNet.Web/Startup.cs
@@ -5,17 +5,39 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using PersonalSiteDotNet.Core.Entities;
+using PersonalSiteDotNet.Core.Interfaces;
+using PersonalSiteDotNet.Core.Services;
+using PersonalSiteDotNet.Infrastructure;
 
 namespace PersonalSiteDotNet.Web
 {
     public class Startup
     {
+        
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<Infrastructure.MainContext>(options => options.UseInMemoryDatabase("Personal Site DB"));
+            services.AddScoped<DbContext, MainContext>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+
+            services.AddControllers();
+            
+            services.AddScoped<IBlogService, BlogService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,14 +48,15 @@ namespace PersonalSiteDotNet.Web
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseHttpsRedirection();
+
             app.UseRouting();
+
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
