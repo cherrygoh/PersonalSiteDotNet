90ef8d2456e31a3371a357543da0f107ad983803
Cherry Goh <cherrycecegoh@gmail.com>
2020-06-09 11:03:31 +0800

    refactor: remove Entries property

--- src/PersonalSiteDotNet.Web/DTO/BlogDTO.cs
+++ src/PersonalSiteDotNet.Web/DTO/BlogDTO.cs
@@ -10,6 +10,5 @@ namespace PersonalSiteDotNet.Web.DTO
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public ICollection<Entry> Entries { get; set; }
     }
 }
