89ead0eaa5d1e470a7e70ee8a435f0feeb9ce327
Cherry Goh <cherrycecegoh@gmail.com>
2020-06-10 00:01:39 +0800

    test: create integration test for blog workflow

---
+++ test/PersonalSiteDotNet.Tests/Integration/BlogTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using PersonalSiteDotNet.Web;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PersonalSiteDotNet.Tests.Integration
+{
+    public class BlogTests: IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+
+        public BlogTests(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task Get_NoParameters_ReturnsAll()
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+            var url = "/api/blog";
+
+            //Act
+            var response = await client.GetAsync(url);
+
+            //Assert
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
