1f46b1ff08c5ab9ab9ddb805e8e720f8fcaaf2be
Cherry Goh <cherrycecegoh@gmail.com>
2020-06-13 21:50:26 +0800

    refactor: move request models to request folder

---
+++ src/PersonalSiteDotNet.Core/Models/Requests/EntryRequestParameters.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonalSiteDotNet.Core.Models
+{
+    public class EntryRequestParameters: RequestParameters
+    {
+        public DateTime? PublishedDateFrom { get; set; } = null;
+        public DateTime? PublishedDateTo { get; set; } = null;
+        public DateTime? ModifiedDateFrom { get; set; } = null;
+        public DateTime? ModifiedDateTo { get; set; } = null;
+        public int? BlogId { get; set; } = null;
+        public IEnumerable<string> TagLabels { get; set; } = null;
+    }
+}
