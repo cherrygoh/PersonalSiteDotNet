6b1063483eb1b2e38f887f1fd0c170ca2e630e59
Cherry Goh <cherrycecegoh@gmail.com>
2020-06-13 21:42:40 +0800

    feat: create request parameter model

---
+++ src/PersonalSiteDotNet.Core/Models/RequestParameters.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonalSiteDotNet.Core.Models
+{
+    public class RequestParameters
+    {
+        // Filter parameters
+        public int? Id { get; set; } = null;
+
+        // Sorting parameters
+        public string sortProperty { get; set; } = null;
+        public bool? sortDesc { get; set; } = null;
+
+        // Paging parameters
+        public int? pageNumber { get; set; } = null;
+        public int? pageSize { get; set; } = null; 
+    }
+}
