d7fbfa7fff1a5123f8eb30d8ff251467152f3838
Cherry Goh <cherrycecegoh@gmail.com>
2020-06-13 21:51:37 +0800

    refactor: move paged result to core project

---
+++ src/PersonalSiteDotNet.Core/Models/PagedResult.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PersonalSiteDotNet.Core.Models
+{
+    public class PagedResult<T>
+    {
+        // Max # of rows/ entries in a page
+        public int PageSize { get; set; }
+        
+        public int PageNumber { get; set; }
+        
+        // Max # of pages
+        public int PageCount { get; set; }
+        
+        // Total number of results from query
+        public int ResultsCount { get; set; }
+        
+        // Total number of results on this page
+        public int RowCount { get; set; }
+        public IEnumerable<T> Results { get; set; } 
+    }
+}
